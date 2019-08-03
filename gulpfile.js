11d1635889049a6beb2e3935c2c8d7d8d13d3abc
sajrus33 <brianwala22@gmail.com>
2019-08-03 13:34:01 +0200

    bookmark added, styles for grid, setting zindex

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,17 @@ const concat = require("gulp-concat");
 const cleanCSS = require("gulp-clean-css");
 const watch = require("gulp-watch");
 
-
-
-gulp.task("watch", function () {
-    watch("./src/css/*.css", () => {
-        return gulp.src(['src/css/start.css', './src/css/header.css', './src/css/today.css', './src/css/week.css', './src/css/day.css', './src/css/media.css'])
-            .pipe(concat("style.css"))
-            .pipe(cleanCSS())
-            .pipe(gulp.dest("./public/css/"));
-    });
+gulp.task("watch", function() {
+  watch("./src/css/*.css", () => {
+    return gulp
+      .src([
+        "./src/css/start.css",
+        "./src/css/header.css",
+        "./src/css/main.css",
+        "./src/css/activities.css"
+      ])
+      .pipe(concat("style.css"))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest("./public/css/"));
+  });
 });
